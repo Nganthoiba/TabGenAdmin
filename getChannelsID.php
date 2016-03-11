3f10c08555d61dfb9d5493b8ed659c6202d658c1
Nganthoiba <leecba@gmail.com>
2016-03-11 22:32:24 +0530

    getting channels
    getting list of channels of a tem in wich the user belongs

--- getChannelsID.php
+++ getChannelsID.php
@@ -1,13 +1,13 @@
 <?php
 /* php code for getting list of channels along with IDs associated with the particular teams which a particular user belongs to */
 
-//if(!empty($_GET['user_id'])){
-//	$user_id = $_GET['user_id'];
+if(!empty($_GET['user_id'])){
+	$user_id = $_GET['user_id'];
 	include('connect_db.php');
 	if($conn){
 		$query = "select Channels.Id as Channel_ID, Channels.DisplayName as Channel_name,Users.Username,Teams.Name as Team_Name
 				from Channels,Users,Teams
-				where Channels.TeamId = Users.TeamId and Teams.Id = Users.TeamId ";
+				where Channels.TeamId = Users.TeamId and Teams.Id = Users.TeamId and Users.Id='$user_id'";
 		$res=$conn->query($query);
 		if($res){
              while($row=$res->fetch(PDO::FETCH_ASSOC)){
