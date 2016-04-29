7e4f454926c2721ce7d93c4696d13e41399f1b36
Nganthoiba <leecba@gmail.com>
2016-04-29 13:36:25 +0530

    getting current message

--- getCurrentPost.php
+++ getCurrentPost.php
@@ -2,12 +2,13 @@
 	//php code for getting post messages
 	$channel_id = $_GET['channel_id'];
 	$token = $_GET['token'];
+	$timestamp = $_GET['timestamp'];
 	
 	include('connect_db.php');
 	include('ConnectAPI.php');
 	include('tabgen_php_functions.php');
 	
-	$url="http://".IP.":8065/api/v1/channels/".$channel_id."/posts/0/20";
+	$url="http://".IP.":8065/api/v1/channels/".$channel_id."/posts/".$timestamp;
 	$getPosts = new ConnectAPI();
 	$result = $getPosts->getDataByToken($url,$token);
 	$decoded_res = json_decode($result);
