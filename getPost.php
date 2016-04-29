987caab637627bab916ffb01c696d338e40ac49e
Nganthoiba <leecba@gmail.com>
2016-04-29 13:00:30 +0530

    changing the for parsing json

--- getPost.php
+++ getPost.php
@@ -7,15 +7,12 @@
 	include('ConnectAPI.php');
 	include('tabgen_php_functions.php');
 	
-	$url="http://".IP.":8065/api/v1/channels/".$channel_id."/posts/0/12";
+	$url="http://".IP.":8065/api/v1/channels/".$channel_id."/posts/0/20";
 	$getPosts = new ConnectAPI();
 	$result = $getPosts->getDataByToken($url,$token);
-	//echo $result;
 	$decoded_res = json_decode($result);
-	$posts=$decoded_res->posts;
-	for($i=0;$i<sizeof($decoded_res->order);$i++){
-		$post_id = $decoded_res->order[$i];
-		$decoded_res->posts->$post_id->no_of_reply=getNoOfReplies($conn,$post_id);	
+	foreach($decoded_res->order as $post_id){
+		$decoded_res->posts->$post_id->no_of_reply=getNoOfReplies($conn,$post_id);
 	}
 	echo json_encode($decoded_res);
 ?>
