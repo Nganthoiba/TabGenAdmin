4f4d7e45c8e6d468dd1826d01e293fa359bca6cc
Nganthoiba <leecba@gmail.com>
2016-05-03 18:20:17 +0530

    adding no of likes

--- like_a_post.php
+++ like_a_post.php
@@ -7,18 +7,22 @@
 	
 	if(isAlreadyLiked($conn,$post_id,$user_id)){
 		if(unlikeAPost($conn,$post_id,$user_id)){
-			echo json_encode(array("post_id"=>$post_id,"liked_status"=>"unliked","message"=>"You have unliked the post."));
+			echo json_encode(array("post_id"=>$post_id,"liked_status"=>"unliked","message"=>"You have unliked the post.",
+			"no_of_likes"=>getNoOfLikes($conn,$post_id)));
 		}
 		else{
-			echo json_encode(array("post_id"=>$post_id,"liked_status"=>"liked","message"=>"Sorry, unable unlike the post."));
+			echo json_encode(array("post_id"=>$post_id,"liked_status"=>"liked","message"=>"Sorry, unable unlike the post.",
+			"no_of_likes"=>getNoOfLikes($conn,$post_id)));
 		}	
 	}
 	else{
 		if(likeAPost($conn,$post_id,$user_id)){
-			echo json_encode(array("post_id"=>$post_id,"liked_status"=>"liked","message"=>"You have liked the post."));
+			echo json_encode(array("post_id"=>$post_id,"liked_status"=>"liked","message"=>"You have liked the post.",
+			"no_of_likes"=>getNoOfLikes($conn,$post_id)));
 		}
 		else{
-			echo json_encode(array("post_id"=>$post_id,"liked_status"=>"unliked","message"=>"Sorry, you could not like the post."));
+			echo json_encode(array("post_id"=>$post_id,"liked_status"=>"unliked","message"=>"Sorry, you could not like the post.",
+			"no_of_likes"=>getNoOfLikes($conn,$post_id)));
 		}
 	}
 ?>
