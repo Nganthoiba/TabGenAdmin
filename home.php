30d1a391cec36e6cafa255ddecd6c3583bba8a5d
Nganthoiba <leecba@gmail.com>
2016-05-05 15:11:57 +0530

    layout

--- home.php
+++ home.php
@@ -930,11 +930,10 @@ Tabs are created in this section-->
 								});
 							});
 						</script>
-						<div id="user_display_content" style="max-width:100%; min-height:50%; overflow:hidden;overflow-x:auto">
+				<div id="user_display_content" style="max-width:100%; min-height:50%; overflow:hidden;overflow-x:auto">
 							Content goes here....
-						</div>
-					</div>
-					<div class="pull-right"><Button class="btn btn-link" id="view_all_users">VIEW ALL</Button></div>	
+				</div>
+				<div class="pull-right"><Button class="btn btn-link" id="view_all_users">VIEW ALL</Button></div>	
 			</div>
 		</div>
 </div>
