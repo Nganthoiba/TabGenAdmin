bc9d1dadf9fd9037af3570e9b74a488e87f813a1
Nganthoiba <leecba@gmail.com>
2016-05-11 13:24:53 +0530

    web service for getting replies of a particular post

---
+++ getReplies.php
@@ -0,0 +1,22 @@
+<?php 
+	//php code for getting replies of a particular post messages
+	
+	$post_id = $_GET['post_id'];
+	include('connect_db.php');
+	include('ConnectAPI.php');
+	include('tabgen_php_functions.php');
+	
+	if($conn){
+			$query = "select * from Posts where where RootId='$post_id' or ParentId='$post_id'";
+			$output=null;
+			$res = $conn->query($query);
+			while($row = $res->fetch(PDO::FETCH_ASSOC)){
+				$output[]=$row;
+			}
+			echo json_encode($output);	
+	}
+	else 
+		echo null;
+		
+		
+?>
