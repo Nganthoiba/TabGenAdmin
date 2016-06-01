26c0a36dd771bfa95f5b557236869dbfaf4770c2
Nganthoiba <leecba@gmail.com>
2016-06-01 12:19:57 +0530

    layout changes

--- home.php
+++ home.php
@@ -82,38 +82,74 @@
                 $user_email= $user_data->email;
 						
 	?>
-    
-	<div id="wrapper">
-		<nav class="navbar navbar-inverse navbar-fixed-top">
-		  <div class="container-fluid">					 
-			<div class="navbar-header"> 
-				<a class="navbar-brand" href="#">
-				  <!--<img src="img/user.png" class="small_circular_image" alt="No profile Image found"/>
-						<a class="btn btn-default" href="#menu-toggle" id="menu-toggle">
-								<span class="glyphicon glyphicon-align-justify"></span>
-						</a>
-				  -->
-					<span class="glyphicon glyphicon-align-justify" href="#menu-toggle" id="menu-toggle"></span>
-				</a>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+	  <div class="container-fluid">
+		<div class="navbar-header">
+		  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		  </button>
+		  <a class="navbar-brand" href="#menu-toggle" id="menu-toggle"><span class="glyphicon glyphicon-align-justify"></span></a>
+		</div>
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		  <ul class="nav navbar-nav">
+			  <!--
+			<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+			<li><a href="#">Link</a></li>
+			<li class="dropdown">
+			  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+			  <ul class="dropdown-menu">
+				<li><a href="#">Action</a></li>
+				<li><a href="#">Another action</a></li>
+				<li><a href="#">Something else here</a></li>
+				<li role="separator" class="divider"></li>
+				<li><a href="#">Separated link</a></li>
+				<li role="separator" class="divider"></li>
+				<li><a href="#">One more separated link</a></li>
+			  </ul>
+			</li>-->
+		  </ul>
+		  <!--<form class="navbar-form navbar-left" role="search">
+			<div class="form-group">
+			  <input type="text" class="form-control" placeholder="Search">
 			</div>
-			<!--
-				<ul class="nav navbar-nav">
+			<button type="submit" class="btn btn-default">Submit</button>
+		  </form>
+		  <ul class="nav navbar-nav">
 				  <li class="active"><a href="#">Home</a></li>
 				  <li><a href="#">Page 1</a></li>
 				  <li><a href="#">Page 2</a></li> 
-				</ul>cdca
-			-->
-			<ul class="nav navbar-nav navbar-right">
-			  <li><a href="#">
-				  <span class="glyphicon glyphicon-user"></span>
-				   Edit Profiles</a>
-			  </li>
-			  <li><a href="#" data-toggle="modal" data-target="#logoutConfirmation">
-				  <span class="glyphicon glyphicon-log-out"></span> logout</a>
-			  </li>
-			</ul>
-		  </div>
-		</nav>
+		  </ul>-->
+		  <ul class="nav navbar-nav navbar-right">
+			<li>
+				<a href="#">
+					  <span class="glyphicon glyphicon-user"></span>
+					   Edit Profiles
+				</a>
+			</li>
+			<li>
+				<a href="#" data-toggle="modal" data-target="#logoutConfirmation">
+					  <span class="glyphicon glyphicon-log-out"></span>&nbsp;logout</a>
+			</li>
+			<!--<li><a href="#">Link</a></li>
+			<li class="dropdown">
+			  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+			  <ul class="dropdown-menu">
+				<li><a href="#">Action</a></li>
+				<li><a href="#">Another action</a></li>
+				<li><a href="#">Something else here</a></li>
+				<li role="separator" class="divider"></li>
+				<li><a href="#">Separated link</a></li>
+				
+			  </ul>
+			</li>-->
+		  </ul>
+		</div>
+	  </div>
+	</nav>
+	<div id="wrapper">
         <!-- Sidebar -->
         <div id="sidebar-wrapper">
 			<center>
