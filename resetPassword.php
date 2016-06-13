4fae4c7e08abcdf42343c8341605edbcb8a1a724
Nganthoiba <leecba@gmail.com>
2016-06-13 16:37:23 +0530

    under development

--- resetPassword.php
+++ resetPassword.php
@@ -17,7 +17,7 @@
 		echo json_encode(array("status"=>false,"message"=>"Unable to connect api for resetting password."));
 	}
 	else if($resetPassword->httpResponseCode==200){
-		echo json_encode(array("status"=>true,"message"=>$result));
+		echo json_encode(array("status"=>true,"message"=>"Under Development...."));
 	}
 	else{
 		$decoded_res = json_decode($result);
