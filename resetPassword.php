23dafb0c354f81814ae152f9ee0907ccb0eaa287
Nganthoiba <leecba@gmail.com>
2016-06-13 17:08:15 +0530

    password reset

--- resetPassword.php
+++ resetPassword.php
@@ -9,15 +9,15 @@
 	include('ConnectAPI.php');
 	include('tabgen_php_functions.php');
 	
-	$url="http://".IP.":8065/api/v1/users/update_password_forcefully";
-	$data=json_encode(array("user_id"=>$user_id,"new_password"=>$new_password));
+	$url="http://".IP.":8065/api/v1/users/reset_password";
+	$data=json_encode(array("user_id"=>$user_id,"new_password"=>$new_password,"name"=>"organisation"));
 	$resetPassword = new ConnectAPI();
 	$result = $resetPassword->sendPostDataWithToken($url,$data,$token);
 	if($resetPassword->httpResponseCode==404){
 		echo json_encode(array("status"=>false,"message"=>"Unable to connect api for resetting password."));
 	}
 	else if($resetPassword->httpResponseCode==200){
-		echo json_encode(array("status"=>true,"message"=>"Under Development.... user token: ".$token));
+		echo json_encode(array("status"=>true,"message"=>"Password has been reset."));
 	}
 	else{
 		$decoded_res = json_decode($result);
