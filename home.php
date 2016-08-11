78964b4dd7949cbf04b22d8b4a46293baa48f0c8
Nganthoiba <leecba@gmail.com>
2016-08-11 22:48:03 +0530

    remove unnecessary dropdown

--- home.php
+++ home.php
@@ -172,18 +172,6 @@
 				<a href="#" data-toggle="modal" data-target="#logoutConfirmation" >
 					  <span class="glyphicon glyphicon-log-out"></span>&nbsp;logout</a>
 			</li>
-			<!--<li><a href="#">Link</a></li>-->
-			<li class="dropdown">
-			  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-			   aria-expanded="false">Dropdown <span class="caret"></span></a>
-			  <ul class="dropdown-menu">
-				<li><a href="#">Action</a></li>
-				<li><a href="#">Another action</a></li>
-				<li><a href="#">Something else here</a></li>
-				<li role="separator" class="divider"></li>
-				<li><a href="#">Separated link</a></li>
-			  </ul>
-			</li>
 		  </ul>
 		</div>
 	  </div>
