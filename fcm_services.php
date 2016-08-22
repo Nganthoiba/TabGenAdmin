0bdf344808efbac61121de4df38dac2bc86f8638
Nganthoiba <leecba@gmail.com>
2016-08-22 12:00:27 +0530

    test fcm

--- fcm_services.php
+++ fcm_services.php
@@ -19,7 +19,7 @@ echo json_encode($tokens);
 function sendFirebasedCloudMessage($fcm_token, $message) {
 	/*fcm_token is a list of tokens*/
 	
-    $apiKey = 'key-AIzaSyAvs3D3fvlWo9tGl-cWt3a5BXRfCgd8nQo';
+    $apiKey = 'AIzaSyAnnGIii3XM3HfV5n8WsFatXluNr8bAiSo';
     //'AIzaSyBXuHwQiXttNBAlQO5sf8899OGK8ZPHbQ4';//'AIzaSyAczj4Or2XXcPX53zc9K2GREP-j9lV3zC8';
     $url = 'https://fcm.googleapis.com/fcm/send';
 	 //'https://android.googleapis.com/gcm/send';
