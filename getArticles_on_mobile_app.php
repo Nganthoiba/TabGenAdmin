8aa358d255cb7127ad95090ba9c5915e4b45c947
Nganthoiba <leecba@gmail.com>
2016-09-05 12:26:53 +0530

    article bookmark

--- getArticles_on_mobile_app.php
+++ getArticles_on_mobile_app.php
@@ -36,7 +36,7 @@
 				$row['external_link_url'] =  "http://".SERVER_IP."/TabGenAdmin/getAnArticleWebView.php?article_id=".$row['Id'];
 				$row['no_of_likes'] = getNoOfLikesOfArticle($conn,$row['Id']);
 				$row['is_bookmarked_by_you']=isArticleAlreadyBookmarked($conn,$row['Id'],$user_id);
-				$row['is_liked_by_you']=isArticleAlreadyLiked($conn,$row['Id'],$user_id);	
+				$row['is_liked_by_you']=!isArticleAlreadyLiked($conn,$row['Id'],$user_id);	
 				$item[]=$row; 
 				$count++;		
 			}
