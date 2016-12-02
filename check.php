7cee1d93a5f56f58aadd1b3bfa7a10e3aa5adad3
Khangenbam Nganthoiba Angom <leecba@gmail.com>
2016-12-02 17:23:32 +0530

    check
    check  if data exists

--- check.php
+++ check.php
@@ -8,7 +8,30 @@ $ipadd = mysqli_real_escape_string($_POST['ipadd']);
 $datusr = mysqli_real_escape_string($_POST['datusr']);
 $datpass = mysqli_real_escape_string($_POST['datpass']);
 
-
+if($conn){
+	$query = "select count(*) as count from HISConnectivity 
+	where OrganisationName='$or' 
+	and DataServerIPAdd = '$ipadd' 
+	and DatabaseUsername = '$datusr'";
+	$result = $conn->query($query);
+	if($result){
+		$row = $result->fetch(PDO::FETCH_ASSOC);
+		$count = (int)$row['count'];
+		if($count>0){
+			echo "Tested OK";
+		}
+		else{
+			echo "No data found with the given details.";
+		}
+	}
+	else{
+		echo "Oops! An error with the query.";
+	}
+}
+else{
+	echo "Failed to connect database";
+}
+/*
 $result = mysqli_query('select OrganisationName,DataServerIPAdd,DatabaseUsername,DatabasePassword from HISConnectivity where values = "'. $or .','. $ipadd .','. $datusr .','. $datpass .'"');  
   
 
@@ -19,5 +42,7 @@ if(mysqli_num_rows($result)>0){
    
   
     echo "error";  
-}  
+} 
+*/
+ 
 ?>
